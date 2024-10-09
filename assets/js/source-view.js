26f240d011a756c938b54a857542d3b96b53d5f7
Ali Çömez <sys@rootali.net>
2024-10-09 04:04:45 +0300

    Create source-view.js

---
+++ assets/js/source-view.js
@@ -0,0 +1,6 @@
+// Kaynak kodu görüntüleyenlere özel mesaj
+if (window.location.href.startsWith("view-source")) {
+    console.log("Kral kaynağa bakıp ne göreceksin orada? bana sorsana merak ettiğin şeyi..");
+} else {
+    console.log("Kardeşim gez dolaş sitedeki içerikler ücretsiz korkma :)");
+}
